427cac47a0a9079e2f4eb4350d2e14ca8b461b9d
Sebastian Robles <tito_segundo@hotmail.com>
2016-07-28 20:23:53 -0430

    no message

--- fakturo.php
+++ fakturo.php
@@ -27,6 +27,7 @@ class fakturo {
 	function __construct() {
 		$this->setupGlobals();
 		
+		$this->loadTextDomain();
 	}
 	private function includes() {
 		
@@ -34,22 +35,46 @@ class fakturo {
 	private function setupGlobals() {
 
 		// Plugin Folder Path
-		if (!defined('WPE_FAKTURO_PLUGIN_DIR')) {
-			define('WPE_FAKTURO_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+		if (!defined('FAKTURO_PLUGIN_DIR')) {
+			define('FAKTURO_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 		}
 
 		// Plugin Folder URL
-		if (!defined('WPE_FAKTURO_PLUGIN_URL')) {
-			define('WPE_FAKTURO_PLUGIN_URL', plugin_dir_url(__FILE__));
+		if (!defined('FAKTURO_PLUGIN_URL')) {
+			define('FAKTURO_PLUGIN_URL', plugin_dir_url(__FILE__));
 		}
 
 		// Plugin Root File
-		if (!defined('WPE_FAKTURO_PLUGIN_FILE')) {
-			define('WPE_FAKTURO_PLUGIN_FILE', __FILE__ );
+		if (!defined('FAKTURO_PLUGIN_FILE')) {
+			define('FAKTURO_PLUGIN_FILE', __FILE__ );
 		}
 
 	}
-	
+	public function loadTextDomain() {
+		// Set filter for plugin's languages directory
+		$lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
+		$lang_dir = apply_filters('fakturo_languages_directory', $lang_dir );
+
+		// Traditional WordPress plugin locale filter
+		$locale        = apply_filters( 'plugin_locale',  get_locale(), 'fakturo' );
+		$mofile        = sprintf( '%1$s-%2$s.mo', 'fakturo', $locale );
+
+		// Setup paths to current locale file
+		$mofile_local  = $lang_dir . $mofile;
+		$mofile_global = WP_LANG_DIR . '/fakturo/' . $mofile;
+
+		if ( file_exists( $mofile_global ) ) {
+			// Look in global /wp-content/languages/fakturo/ folder
+			load_textdomain( 'fakturo', $mofile_global );
+		} elseif ( file_exists( $mofile_local ) ) {
+			// Look in local /wp-content/plugins/fakturo/languages/ folder
+			load_textdomain( 'fakturo', $mofile_local );
+		} else {
+			// Load the default language files
+			load_plugin_textdomain( 'fakturo', false, $lang_dir );
+		}
+		
+	}
 }
 
 endif; // End if class_exists check
@@ -62,6 +87,5 @@ function getClassFakturo() {
 	}
 	return $fakturo;
 }
-
 getClassFakturo();
 ?>
