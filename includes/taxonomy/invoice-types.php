54cf07264eca84f647389091b79647f7c307ab37
Esteban <etruel@etruel.com>
2016-09-01 00:10:00 -0300

    fix menu y submenu a "Settings" para invoice-type taxonomy

--- includes/taxonomy/invoice-types.php
+++ includes/taxonomy/invoice-types.php
@@ -16,6 +16,8 @@ class fktr_tax_nvoice_types {
 		add_action(self::$tax_name.'_edit_form_fields', array(__CLASS__, 'edit_form_fields'));
 		add_action(self::$tax_name.'_add_form_fields',  array(__CLASS__, 'add_form_fields'));
 		
+		add_filter('parent_file',  array( __CLASS__, 'tax_menu_correction'));
+		add_filter('submenu_file',  array( __CLASS__, 'tax_submenu_correction'));
 		
 		add_action('edited_'.self::$tax_name, array(__CLASS__, 'save_fields'), 10, 2);
 		add_action('created_'.self::$tax_name, array(__CLASS__,'save_fields'), 10, 2);
@@ -25,8 +27,25 @@ class fktr_tax_nvoice_types {
 		
 		add_action('admin_enqueue_scripts', array(__CLASS__, 'scripts'), 10, 1);
 		
-		
 	}
+	// highlight the proper top level menu
+	static function tax_menu_correction($parent_file) {
+		global $current_screen;
+		if ($current_screen->id == "edit-fktr_invoice_types") {
+			$parent_file = 'fakturo_dashboard';
+		}
+		return $parent_file;
+	}
+	
+	// highlight the proper sub level menu
+	static function tax_submenu_correction($submenu_file) {
+		global $current_screen;
+		if ($current_screen->id == "edit-fktr_invoice_types") {
+			$submenu_file = 'fakturo-settings';
+		}
+		return $submenu_file;
+	}
+	
 	public static function init() {
 		$labels = array(
 			'name'                       => _x( 'Invoice Types', 'Invoice Types' ),
