f5da8c2beaf6df9ced2935be135fc67f01ffec60
sniuk <tito_segundo@hotmail.com>
2016-12-22 12:00:49 -0400

    docs checks and receipt

---
+++ docs/edit-fktr_check-help.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Fakturo description of Help Texts Array
+ * -------------------------------
+ * array('Text for left tab link' => array(
+ * 	'field_name' => array( 
+ * 		'title' => 'Text showed as bold in right side' , 
+ * 		'tip' => 'Text html shown below the title in right side and also can be used for mouse over tips.' , 
+ * 		'plustip' => 'Text html added below "tip" in right side in a new paragraph.',
+ * )));
+ */
+
+$helptexts = array( 
+	'MODEL' => array( 
+		'Item1' => array( 
+			'title' => __('Concept','fakturo'),
+			'tip' => __('Form that allows you to classify products through mechanized processes. In order to add a new model, just click on the button <b>"Add New Model"</b> to save its name and description.','fakturo'))
+	),
+
+);
+
+
+
+?>
---
+++ docs/fktr_receipt-help.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fakturo description of Help Texts Array
+ * -------------------------------
+ * array('Text for left tab link' => array(
+ * 	'field_name' => array( 
+ * 		'title' => 'Text showed as bold in right side' , 
+ * 		'tip' => 'Text html shown below the title in right side and also can be used for mouse over tips.' , 
+ * 		'plustip' => 'Text html added below "tip" in right side in a new paragraph.',
+ * )));
+ */
+
+
+$helptexts = array( 
+	'PRINT FORMATS' => array( 
+		'item1' => array( 
+			'title' => __('Print Template','fakturo'),
+			'tip' => __('In this field, you can generate the system report or reports assigned to a specific module by entering the template name, description and assigment module.','fakturo'),
+		),
+	),
+);
+
+
+
+
+?>
