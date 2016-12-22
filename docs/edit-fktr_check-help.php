b7fdb1903bd3fe36454758b2b72d82ccaf8bca06
Alberto Vargas <avargas@frontuari.com>
2016-12-22 15:14:13 -0400

    Agregando ayudas en lineas de recepcion y cheques

--- docs/edit-fktr_check-help.php
+++ docs/edit-fktr_check-help.php
@@ -16,6 +16,29 @@ $helptexts = array(
 			'title' => __('Concept','fakturo'),
 			'tip' => __('Form that allows you to classify products through mechanized processes. In order to add a new model, just click on the button <b>"Add New Model"</b> to save its name and description.','fakturo'))
 	),
+	'FIELDS' => array( 
+		'Item1' => array( 
+			'title' => __('Serial Number','fakturo'),
+			'tip' => __('Combination of numbers with which the check is identified.','fakturo')),
+		'Item2' => array( 
+			'title' => __('Customer','fakturo'),
+			'tip' => __('Select the customer who owns the check. If they don’t exist, you will have to register them in the customers form. ','fakturo')),
+		'Item3' => array( 
+			'title' => __('Bank','fakturo'),
+			'tip' => __('Banking entity from which the check is issued. ','fakturo')),
+		'Item4' => array( 
+			'title' => __('Amount','fakturo'),
+			'tip' => __('Amount of the check.','fakturo')),
+		'Item5' => array( 
+			'title' => __('Currency','fakturo'),
+			'tip' => __('Type of currency of the check. It can be Argentinian peso, dollars or Euros.','fakturo')),
+		'Item6' => array( 
+			'title' => __('Date / Collection Date','fakturo'),
+			'tip' => __('Date of the check’s creation as well as the date to collect from it.','fakturo')),
+		'Item7' => array( 
+			'title' => __('Status','fakturo'),
+			'tip' => __('Select the status in which is the check is found; it can be for deposit, payment charges or rejected.','fakturo'))
+	),
 
 );
 
--- docs/fktr_receipt-help.php
+++ docs/fktr_receipt-help.php
@@ -18,6 +18,32 @@ $helptexts = array(
 			'tip' => __('In this field, you can generate the system report or reports assigned to a specific module by entering the template name, description and assigment module.','fakturo'),
 		),
 	),
+	'DATA RECEPTION' => array( 
+		'item1' => array( 
+			'title' => __('Customer','fakturo'),
+			'tip' => __('Select who will receive the receipt from the customer list. If they are not found, then you must register the customer on the <b>"customers"</b> form. If the customer does exist and doesn’t appear, verify that the option <b>"ACTIVE CUSTOMER"</b> is selected in the customer form.','fakturo'),
+		),
+		'item2' => array( 
+			'title' => __('Payment Method','fakturo'),
+			'tip' => __('Choose the type of payment to be used, whether cash, check or bank transfer','fakturo'),
+		),
+		'item3' => array( 
+			'title' => __('Receipt Currency','fakturo'),
+			'tip' => __(': Currency which will be displayed on the receipt. It can be Argentinian peso, dollars or Euros.','fakturo'),
+		),
+		'item4' => array( 
+			'title' => __('Cash','fakturo'),
+			'tip' => __('Money in the form of paper bills or banknotes, not saved on debit or credit card.','fakturo'),
+		),
+		'item5' => array( 
+			'title' => __('Check','fakturo'),
+			'tip' => __('Accounting document authorized by the customer to extract money due to some purchase.  You can enter one or more checks onto the receipt by clicking <b>"Add Check".</b> Only the checks originating from the customer will appear, and you will be able to search for it through the bank, the banking entity and serial number, to know the amount, the type of currency and the date of collection.','fakturo'),
+		),
+		'item6' => array( 
+			'title' => __('Invoices','fakturo'),
+			'tip' => __('Invoices are added to the customer automatically, calculating the total amount to be paid according to the invoice number, which is associated with the receipt to be created (the invoice number must be created first).','fakturo'),
+		),
+	)
 );
 
 
