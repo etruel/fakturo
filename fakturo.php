6155a7d8c7357f40dd15130207eb0c41340e176b
Esteban <etruel@etruel.com>
2018-10-17 21:23:53 -0300

    version number

--- fakturo.php
+++ fakturo.php
@@ -3,7 +3,7 @@
  Plugin Name: Fakturo
  Plugin URI: http://www.fakturo.org
  Description: Make invoices with products and clients.  If you like it, please rate it 5 stars.
- Version: 0.8 Beta
+ Version: 0.9 Beta
  Author: etruel <esteban@netmdp.com>
  Author URI: http://www.netmdp.com
  */
@@ -11,7 +11,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Plugin version
-if ( ! defined('WPE_FAKTURO_VERSION' ) ) define('WPE_FAKTURO_VERSION', '0.8' ); 
+if ( ! defined('WPE_FAKTURO_VERSION' ) ) define('WPE_FAKTURO_VERSION', '0.9' ); 
 
 if ( ! class_exists( 'fakturo' ) ) :
 
