793c12e36d29965b2d0ed8423c322e9451611111
khaztiel <mage2611@gmail.com>
2021-09-29 16:04:56 -0400

    Print template help tab styles

--- docs/fktr_print_template-help.php
+++ docs/fktr_print_template-help.php
@@ -38,105 +38,119 @@ $helptexts = array(
 		'item1' => array(
 			'title' => __('Functions, Variables & Keywords', 'fakturo'),
 			'tip' => '          
-			<p><strong>Variable example</strong>
-			{* all code between noparse tags is not compiled *}<br/>
-			<tt>variable {noparse}{$variable}{/noparse} = <b>{$variable}</b></tt>
-			</p>
 
-			<p><strong>Variable assignment</strong>
-			<tt>assignment {$number=10} and print {$number}</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Variable example</strong>
+				{* all code between noparse tags is not compiled *}</p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">variable {noparse}{$variable}{/noparse} = <b>{$variable}</b></tt>
+			</div>
 			
-			<p><strong>Operation with strings</strong>
-			<tt>
+
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Variable assignment</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">assignment {$number=10} and print {$number}</tt>
+			</div>
+			
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Operation with strings</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
 				{$variable . $number}<br/>
 				{$number + 20}<br/>
-			</tt>
-			</p>
+				</tt>
+			</div>
 			
-			<p><strong>Variable Modifiers</strong>
-			<tt>
-				{$variable|substr:0,7}<br/>
-				a modifier on string: {"hello world"|strtoupper}<br/>
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Variable Modifiers</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+					{$variable|substr:0,7}<br/>
+					a modifier on string: {"hello world"|strtoupper}<br/>
+				</tt>
+			</div>
 			
-			<p><strong>Global variables</strong>
-			<tt>The variable is declared as global into the PHP {$GLOBALS.global_variable}</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Global variables</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">The variable is declared as global into the PHP {$GLOBALS.global_variable}</tt>
+			</div>
 			
-			<p><strong>Show all declared variables</strong>
-			To show all declared variable use {noparse}{$template_info}{/noparse}.<br/>
-			<tt>
-				{$template_info}<br/>
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Show all declared variables</strong>
+				To show all declared variable use {noparse}{$template_info}{/noparse}.</p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+					{$template_info}<br/>
+				</tt>
+			</div>
 			
-			<p><strong>Constant</strong>
-			<tt>Constant: {#true#}</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Constant</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">Constant: {#true#}</tt>
+			</div>
 			
-			<p><strong>Modier on constant as follow</strong>
-			<tt>Negation of false is true: {PHP_VERSION|round}</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Modier on constant as follow</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">Negation of false is true: {PHP_VERSION|round}</tt>
+			</div>
 
-			<p><strong>Simple loop example</strong>
-			<tt>
-			<ul>
-			{loop="week"}<br/>
-				<li>{$key} = {$value}</li>
-			{/loop}
-			</ul>
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Simple loop example</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+				{loop="week"}<br/>
+					<span style="padding-left: 20px;">{$key} = {$value}</span><br/>
+				{/loop}
+				</tt>
+			</div>
 
-			<p><strong>Loop example with associative array</strong>
-			<tt>
-			<ul>
-				<li>ID _ Name _ Color</li>
-				{loop="user"}<br/>
-					<li class="color{$counter%2+1}">{$key}) - {$value.name|strtoupper} - {$value.color}</li>
-				{/loop}<br/>
-			</ul>
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Loop example with associative array</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+					ID _ Name _ Color<br/>
+					{loop="user"}<br/>
+						<span class="color{$counter%2+1}" style="padding-left: 20px;">{$key}) - {$value.name|strtoupper} - {$value.color}</span><br/>
+					{/loop}<br/>
+				</tt>
+			</div>
 
-			<p><strong>Loop an empty array</strong>
-			<tt>
-			<ul>
-				{loop="empty_array"}<br/>
-					<li class="color{$counter%2+1}">{$key}) - {$value.name} - {$value.color}</li>
-				{else}<br/>
-					<b>The array is empty</b>
-				{/loop}<br/>
-			</ul>
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Loop an empty array</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+					{loop="empty_array"}<br/>
+						<span class="color{$counter%2+1}" style="padding-left: 20px;">{$key}) - {$value.name} - {$value.color}</span><br/>
+					{else}<br/>
+						<b>The array is empty</b>
+					{/loop}
+				</tt>
+			</div>
 			
-			<p><strong>simple if example</strong>
-			<tt>
-			{if="$number==10"}OK!<br/>
-			{else}NO!{/if}<br/>
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>simple if example</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+					{if="$number==10"}OK!<br/>
+					{else}NO!{/if}
+				</tt>
+			</div>
 			
-			<p><strong>example of if, elseif, else example</strong>
-			<tt>
-			{if="substr($variable,0,1)==\'A\'"}First character is A<br>
-			{elseif="substr($variable,0,1)==\'B\'"}First character is B<br>
-			{else}First character of variable is not A neither B<br>
-			{/if}
-			</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>example of if, elseif, else example</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">
+					{if="substr($variable,0,1)==\'A\'"}<br>
+					<span style="padding-left: 20px;">First character is A</span><br>
+					{elseif="substr($variable,0,1)==\'B\'"}<br>
+					<span style="padding-left: 20px;">First character is B</span><br>
+					{else}<br>
+					<span style="padding-left: 20px;">First character of variable is not A neither B</span><br>
+					{/if}
+				</tt>
+			</div>
 			
-			<p><strong>use of ? : operator (number==10?\'OK!\':\'no\')</strong>
-			You can also use the ? operator instead of if<br/>
-			<tt>{$number==10? \'OK!\' : \'no\'}</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>use of ? : operator (number==10?\'OK!\':\'no\')</strong>
+				You can also use the ? operator instead of if</p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">{$number==10? \'OK!\' : \'no\'}</tt>
+			</div>
 			
-			<p><strong>Example of function: ucfirst(strtolower($title))</strong>
-			<tt>{function="ucfirst(strtolower($title))"}</tt>
-			</p>
+			<div style="margin-bottom: 1.5em;">
+				<p style="margin-bottom: .5em"><strong>Example of function: ucfirst(strtolower($title))</strong></p>
+				<tt style="display: block; background-color: #dfe7f1; padding: 10px; max-width: max-content;">{function="ucfirst(strtolower($title))"}</tt>
+			</div>
 			',
 		),
 	),
