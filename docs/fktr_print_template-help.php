6f74b8872d19eaf1350907a8ce8dcfeac246ccfe
Esteban <etruel@etruel.com>
2021-09-29 12:38:26 -0300

    Demo image on TPL for pending invoices

--- docs/fktr_print_template-help.php
+++ docs/fktr_print_template-help.php
@@ -15,8 +15,14 @@ $helptexts = array(
 	'PRINT FORMATS' => array( 
 		'tabtitle' =>  __('Print Formats', 'fakturo' ),
 		'item1' => array( 
-			'title' => __('Print Template','fakturo'),
-			'tip' => __('You can add the default print templates format by assigning it to a specific module by entering the name of the template and the description, these formats will be used when printing invoices or reports.','fakturo'),
+			'title' => __('Print Templates','fakturo'),
+			'tip' => __('They are used to format the different documents to be printed, such as invoices, receipts, reports, etc.','fakturo'),
+            'plustip' => __('Standard HTML is used with the system variables in the format used by the RainTPL library.', 'fakturo')
+		),
+		'item2' => array( 
+			'title' => __('Reset to Default','fakturo'),
+			'tip' => __('Fakturo includes standard templates for almost all the documents handled by it.','fakturo'),
+            'plustip' => __('You can add the default print templates after assigning it to a specific module and entering the template name and description.', 'fakturo')
 		),
 	),
 );
