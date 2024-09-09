cd3b99dd98cc01bac9cb20fe34c3a8b097da324b
samuelalvaradodev <samuelalvaradodev@gmail.com>
2024-09-09 15:17:48 -0400

    change factor

--- assets/js/post-type-products.js
+++ assets/js/post-type-products.js
@@ -1,10 +1,10 @@
 var DefaultMaskNumbers = '';
 var previusValue = 0;
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
 
 	// This code prevents the URL from being filled with the wp-post-new-reload value
-	$('#publish').click(function(e) {
+	$('#publish').click(function (e) {
 		if ($('#original_post_status').val() === 'auto-draft' && window.history.replaceState) {
 			var location;
 			location = window.location.href;
@@ -36,52 +36,52 @@ jQuery(document).ready(function($) {
 	jQuery('.prices_final').mask(DefaultMaskNumbers, { reverse: true });
 	jQuery('#currency').select2();
 
-	
 
-	jQuery('#cost').change(function() {
-		jQuery('.pricestr').each(function(i, obj) {
+
+	jQuery('#cost').change(function () {
+		jQuery('.pricestr').each(function (i, obj) {
 			var porcent = parseFloat(jQuery(this).data('porcentage'));
 			var cost = parseFloat(converMaskToStandar(jQuery('#cost').val(), products_object));
 			var porcentTax = parseFloat(getPorcentOfTaxSelected());
-	
-			
-			var currentPriceVal = parseFloat(jQuery('#prices_' + jQuery(this).data('id')).val()); 
-			var currentFinalPriceVal = parseFloat(jQuery('#prices_final_' + jQuery(this).data('id')).val()); 
-	
-			 newPrice = cost + ((cost / 100) * porcent);
-			
-		if (currentPriceVal === 0 || newPrice === 0) {
+
+
+			var currentPriceVal = parseFloat(jQuery('#prices_' + jQuery(this).data('id')).val());
+			var currentFinalPriceVal = parseFloat(jQuery('#prices_final_' + jQuery(this).data('id')).val());
+
 			newPrice = cost + ((cost / 100) * porcent);
-		} else if (newPrice !== currentPriceVal) {
-			newPrice = currentPriceVal;
-		}
-	
+			jQuery('#suggested_' + jQuery(this).data('id')).html(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
+			if (currentPriceVal === 0 || newPrice === 0) {
+				newPrice = cost + ((cost / 100) * porcent);
+			} else if (newPrice !== currentPriceVal) {
+				newPrice = currentPriceVal;
+			}
+
 			if (newPrice !== currentPriceVal) {
 				jQuery('#prices_' + jQuery(this).data('id')).val(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
 				jQuery('#suggested_' + jQuery(this).data('id')).html(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
 			}
-	
+
 			newPrice = newPrice + ((newPrice / 100) * porcentTax);
-	
-			
-		if (currentFinalPriceVal === 0 || newPrice === 0) {
-			newPrice = cost + ((cost / 100) * porcent) + ((newPrice / 100) * porcentTax);
-		} else if (newPrice !== currentFinalPriceVal) {
-			newPrice = currentFinalPriceVal;
-		}
-	
+
+
+			if (currentFinalPriceVal === 0 || newPrice === 0) {
+				newPrice = cost + ((cost / 100) * porcent) + ((newPrice / 100) * porcentTax);
+			} else if (newPrice !== currentFinalPriceVal) {
+				newPrice = currentFinalPriceVal;
+			}
+
 			if (newPrice !== currentFinalPriceVal) {
 				jQuery('#prices_final_' + jQuery(this).data('id')).val(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
 			}
 		});
 	});
 
-	
-	
-	jQuery('.pricestr').each(function(i, obj) {
+
+
+	jQuery('.pricestr').each(function (i, obj) {
 
 		var porcent = parseFloat(jQuery(this).data('porcentage'));
-			var cost = parseFloat(converMaskToStandar(jQuery('#cost').val(), products_object));
+		var cost = parseFloat(converMaskToStandar(jQuery('#cost').val(), products_object));
 
 		previusValue = parseFloat(converMaskToStandar(jQuery('#prices_final_' + jQuery(this).data('id')).val(), products_object));
 		var newPrice = cost + ((cost / 100) * porcent);
@@ -89,48 +89,47 @@ jQuery(document).ready(function($) {
 	});
 
 
-	jQuery('#tax').change(function() {
-		
-		jQuery('.pricestr').each(function(i, obj) {
+	jQuery('#tax').change(function () {
+
+		jQuery('.pricestr').each(function (i, obj) {
 			var porcent = parseFloat(jQuery(this).data('porcentage'));
 			var cost = parseFloat(converMaskToStandar(jQuery('#cost').val(), products_object));
 			var porcentTax = parseFloat(getPorcentOfTaxSelected());
-	
+
 			var currentPriceVal = parseFloat(converMaskToStandar(jQuery('#prices_' + jQuery(this).data('id')).val(), products_object));
 			var currentFinalPriceVal = parseFloat(converMaskToStandar(jQuery('#prices_final_' + jQuery(this).data('id')).val(), products_object));
-	
-			
+
+
 			var newPrice = cost + ((cost / 100) * porcent);
-			
+
 			if (currentPriceVal === 0 || newPrice === 0) {
 				newPrice = cost + ((cost / 100) * porcent);
 			} else if (newPrice !== currentPriceVal) {
 				newPrice = currentPriceVal;
 			}
-	
+
 			if (newPrice !== currentPriceVal) {
 				jQuery('#prices_' + jQuery(this).data('id')).val(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
 				jQuery('#suggested_' + jQuery(this).data('id')).html(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
 			}
-	
-			if (porcentTax !== 0){
+
+			if (porcentTax !== 0) {
 				newPrice = currentFinalPriceVal + ((currentFinalPriceVal / 100) * porcentTax);
-			}else {
-			newPrice = previusValue - ((previusValue / 100) * porcentTax);
+			} else {
+				newPrice = previusValue - ((previusValue / 100) * porcentTax);
 			}
-			
+
 			if (currentFinalPriceVal === 0 || newPrice === 0) {
 				newPrice = cost + ((cost / 100) * porcent) + ((newPrice / 100) * porcentTax);
-			} 
-	
-			// Actualizar el valor en el input de precio final si es diferente
+			}
+
 			if (newPrice !== currentFinalPriceVal) {
 				jQuery('#prices_final_' + jQuery(this).data('id')).val(newPrice.formatMoney(products_object.decimal_numbers, products_object.decimal, products_object.thousand));
 			}
 		});
 	});
-	
-	
+
+
 });
 
 function getPorcentOfTaxSelected() {
@@ -158,7 +157,7 @@ function converMaskToStandar(valueMasked, maskObject) {
 	}
 	return valueMasked;
 }
-Number.prototype.formatMoney = function(c, d, t) {
+Number.prototype.formatMoney = function (c, d, t) {
 	var n = this,
 		c = isNaN(c = Math.abs(c)) ? 2 : c,
 		d = d == undefined ? "." : d,
